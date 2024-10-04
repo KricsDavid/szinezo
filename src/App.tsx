138438434ac68aff9fef1a1c95fdd650bcc498e4
KricsDavid <david@kricsfalusi.hu>
2024-10-04 08:38:47 +0200

    szinezo

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Color from "./Color"
+
+
+function App() {
+  
+
+  return (
+    <>
+<div className="maingrid">
+<Color/>
+<Color/>
+<Color/>
+
+<Color/>
+<Color/>
+<Color/>
+
+</div>
+
+      <Color/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Color.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Color = () => {
+  return (
+    <button className='colorbtn'></button>
+
+    
+  )
+}
+
+export default Color
