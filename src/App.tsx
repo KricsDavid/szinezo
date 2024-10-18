b84e574221e6fef6f97305997f5c3dcbe38ab6e0
KricsDavid <david@kricsfalusi.hu>
2024-10-18 08:10:34 +0200

    nt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import Color from "./Color"
-import Modal from "./Modal";
-import ColorGuesser from "./ColorGuesser"
+
 function App() {
-  let li = []
 
-  
-  }
-
-return (
-  
-<ColorGuesser></ColorGuesser>
-
-   // <>
-    //<Modal />
-//<div className="maingrid">
-//{li.map(idx => <Color key={idx}/>)}
-
-//</div>
-
-</>
-    
+  return (
+    <>
+    </>
   )
+
 }
+
+
+
 
 export default App
